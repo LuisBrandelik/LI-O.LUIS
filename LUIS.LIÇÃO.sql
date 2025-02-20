71a71e238d3361695e1a30c64a4cfd64d2e56d37
Luis <vegetauniterforever@gmail.com>
2025-02-20 19:55:12 -0300

    Add files via upload

---
+++ LUIS.LIÇÃO.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE bd_escola;
+
+use bd_escola;
+
+CREATE TABLE tb_aluno (
+  cd_aluno INT AUTO_INCREMENT PRIMARY KEY,
+  nm_aluno VARCHAR(100),
+  cpf VARCHAR(11)
+);
+
+CREATE USER 'LuisBrandelik '@'localhost' IDENTIFIED BY '190305';
+GRANT ALL PRIVILEGES ON bd_escola.* TO 'LuisBrandelik '@'localhost';
+
+CREATE USER 'Graciete'@'localhost' IDENTIFIED BY 'senha_graciete';
+GRANT INSERT ON bd_escola.tb_aluno TO 'Graciete'@'localhost';
+
+CREATE USER 'JoseCarlos'@'localhost' IDENTIFIED BY 'senha_josecarlos';
+GRANT INSERT, UPDATE ON bd_escola.tb_aluno TO 'JoseCarlos'@'localhost';
